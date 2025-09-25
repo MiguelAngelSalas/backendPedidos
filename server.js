35fa217ee80df549450a2a72bb3eb4226b213c0a
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-09-25 18:10:13 -0300

    123123123

--- server.js
+++ server.js
@@ -147,6 +147,7 @@ app.post('/api/pedidos', async (req, res) => {
 });
 
 // ✅ Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
+
