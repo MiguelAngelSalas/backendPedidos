bdacd2cbc13793dabd8a75ccf68185408805df80
MiguelAngelSalas <miguelsalas19941@gmail.com>
2025-10-02 20:13:53 -0300

    A ver si ahora si

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3001;
 
+console.log("🌐 process.env.PORT:", process.env.PORT);
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Backend escuchando en el puerto ${PORT}`);
 });
