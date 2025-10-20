2a1688e6df36ea604d43e889e289cedebef1ca0b
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-10-20 15:47:59 -0300

    Refactor app.js to remove duplicate CORS setup

--- app.js
+++ app.js
@@ -21,10 +21,6 @@ app.use(cors({
   credentials: true
 }));
 
-
-app.use(cors());
-
-
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -40,5 +36,6 @@ app.get("/", (req, res) => {
 app.use(errorHandler);
 
 module.exports = app;
+
 
 
