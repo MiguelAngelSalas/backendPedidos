a11591f908fcb9f29ff6800c3d984b4d5e636cc1
Miguel Angel Salas <miguelsalas19941@gmail.com>
2025-10-28 06:52:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,30 @@ const errorHandler = require("./src/middlewares/errorHandler");
 
 const app = express();
 
-app.use(cors());
+// Orígenes permitidos
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://impresionesatucasa.com.ar"
+];
 
+// Configuración de CORS
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS bloqueado para este origen"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+// Manejo de preflight (OPTIONS)
+app.options("*", cors());
+
+// Middleware para parsear JSON y formularios
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,9 +44,3 @@ app.get("/", (req, res) => {
 app.use(errorHandler);
 
 module.exports = app;
-
-
-
-
-
-
